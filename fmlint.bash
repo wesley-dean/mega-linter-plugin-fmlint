aaecd2bbfc2b4d773a59fe45f24cf0e7f6ee2146
Wes Dean <wesdean@kdaweb.com>
2025-04-11 11:10:11 -0400

    Munge parameter formatting for fmlint

--- fmlint.bash
+++ fmlint.bash
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-filename="$1"
+# shellcheck disable=SC2124
+filename="${@: -1}"
 
 if [ ! -f "$filename" ] ; then
   yamllint "$@"
   exit $?
 fi
 
-shift
+# This ugliness is to remove the last argument from the list of arguments
+set -- "${@:1:$(($#-1))}"
 
 tempfile="$(mktemp)"
-
 sed -Eze 's/((^|\n)---.*?---(\n|$)).*/\1/' < "$filename" > "$tempfile"
 
 yamllint "$@" "$tempfile" | sed -Ee "s|${tempfile}|${filename}|g"
+
+rm -f "$tempfile"
