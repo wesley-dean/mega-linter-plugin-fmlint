11be0fbc9490dfba09db1eb5f512b3d5286ec50a
Wes Dean <wesdean@kdaweb.com>
2025-04-11 11:47:44 -0400

    Refactor to eliminate temp file

--- fmlint.bash
+++ fmlint.bash
@@ -3,17 +3,13 @@
 # shellcheck disable=SC2124
 filename="${@: -1}"
 
-if [ ! -f "$filename" ] ; then
+if [ -f "$filename" ]; then
+  # This ugliness is to remove the last argument from the list of arguments
+  set -- "${@:1:$(($#-1))}"
+
+  sed -Eze 's/((^|\n)---.*?---(\n|$)).*/\1/' < "$filename" \
+  | yamllint "$@" -
+else
   yamllint "$@"
-  exit $?
 fi
 
-# This ugliness is to remove the last argument from the list of arguments
-set -- "${@:1:$(($#-1))}"
-
-tempfile="$(mktemp)"
-sed -Eze 's/((^|\n)---.*?---(\n|$)).*/\1/' < "$filename" > "$tempfile"
-
-yamllint "$@" "$tempfile" | sed -Ee "s|${tempfile}|${filename}|g"
-
-rm -f "$tempfile"
