1780e1fa1356a20b49379e28a43800719d5f053b
Wes Dean <wesdean@kdaweb.com>
2025-04-11 10:59:41 -0400

    Check for file existence for --version

--- fmlint.sh
+++ fmlint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 filename="$1"
+
+if [ ! -f "$filename" ] ; then
+  yamllint "$@"
+  exit $?
+fi
+
+shift
+
 tempfile="$(mktemp)"
 
 sed -Eze 's/((^|\n)---.*?---(\n|$)).*/\1/' < "$filename" > "$tempfile"
 
-yamllint "$tempfile" | sed -Ee "s|${tempfile}|${filename}|g"
+yamllint "$@" "$tempfile" | sed -Ee "s|${tempfile}|${filename}|g"
