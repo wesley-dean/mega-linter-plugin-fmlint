ffab5b52a8432629961341cddfec421039fba89d
Wes Dean <wesdean@kdaweb.com>
2025-04-11 10:54:34 -0400

    Refector download

--- test/megalinter.bash
+++ test/megalinter.bash
@@ -11,7 +11,7 @@ docker run \
   -e REPORT_OUTPUT_FOLDER=none \
   -e DISABLE_ERRORS=true \
   -e PRINT_ALPACA=false \
-  -e PLUGINS="[\"file://mega-linter-plugin-dclint/dclint.megalinter-descriptor.yml\"]" \
-  -e ENABLE_LINTERS="[\"DOCKERFILE_DCLINT\"]" \
-  -e MEGALINTER_FILES_TO_LINT="[\"test/docker-compose-working.yml\",\"test/docker-compose-bad.yml\"]" \
+  -e PLUGINS="[\"file://mega-linter-plugin-frontmatter-linter/frontmatter-linter.megalinter-descriptor.yml\"]" \
+  -e ENABLE_LINTERS="[\"MARKDOWN_FRONTMATTER_LINTER\"]" \
+  -e MEGALINTER_FILES_TO_LINT="[\"test/good.yml\",\"test/bad.md\"]" \
   docker.io/oxsecurity/megalinter-ci_light:latest
